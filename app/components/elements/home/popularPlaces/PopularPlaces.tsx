5444031e287621f27b9194b4610649e7f4409a7b
AnnaDe-v <annade.v88@gmail.com>
2022-04-23 20:02:19 +0300

    added places

---
+++ app/components/elements/home/popularPlaces/PopularPlaces.tsx
@@ -0,0 +1,47 @@
+import styles from './PopularPlaces.module.scss';
+import {FC} from "react";
+import {IPlace} from "../../../../types/place";
+import Link from 'next/link';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css'
+
+interface IPopularPlaces {
+    places: IPlace[]
+    isLoading: boolean
+}
+
+const PopularPlaces: FC<IPopularPlaces> = ({places, isLoading}) => {
+    return (
+        <div className={styles.wrapper}>
+            <h2>Popular places</h2>
+            {
+                isLoading ? (<Skeleton
+                        count={1}
+                        height={200}
+                        borderRadius={20}
+                        baseColor='#161617'
+                        highlightColor='#fff'
+                        />) : places.length ?
+
+                    ( places.map(place => (
+                <Link href={`place/${place.slug}`}
+                      key={place.slug}
+                >
+                    <a
+
+                        className={styles.item}
+                        style={{backgroundImage: `url(${place.imageLink})`}}
+                    >
+                        <span className={styles.heading}>
+                            {place.location.city + ', ' + place.location.country}
+                        </span>
+                    </a>
+                </Link>
+            ))
+                    ) : <div style={{color: '#fff'}}>Not found</div>}
+        </div>
+
+
+    )
+}
+export default PopularPlaces;
