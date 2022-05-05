ced4c265c6fe8ed491467ab867131bc65e25da77
AnnaDe-v <annade.v88@gmail.com>
2022-05-05 19:40:33 +0300

    utils for clear text

--- app/utils/Meta.tsx
+++ app/utils/Meta.tsx
@@ -1,40 +1,44 @@
-import React, {FC} from 'react';
-import {Head} from "next/head";
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import { FC } from 'react'
+import { onlyText } from './clearText'
 
 interface IMeta {
     title: string
     description?: string
+    image?: string
 }
 
-const Meta: FC<IMeta> = ({title, description}) => {
-    return (
-        <div>
-            <Head>
-                <title itemProp='headline'>{title} | Travel App</title>
-                {description ? (
-                    <>
-                        <meta
-                            itemProp='description'
-                            name='description'
-                            content={onlyText(description, 152)}
-                        />
-                        <link rel='canonical' href={currentUrl} />
-                        <meta property='og:locale' content='en' />
-                        <meta property='og:title' content={title} />
-                        <meta property='og:url' content={currentUrl} />
-                        <meta property='og:image' content={image || '/logo.jpg'} />
-                        <meta property='og:site_name' content='Travel App' />
-                        <meta
-                            property='og:description'
-                            content={onlyText(description, 197)}
-                        />
-                    </>
-                ) : (
-                    <meta name='robots' content='noindex, nofollow' />
-                )}
-            </Head>
-        </div>
-    );
-};
+const Meta: FC<IMeta> = ({ title, description, image }) => {
+    const { asPath } = useRouter()
+    const currentUrl = `${process.env.APP_URL}${asPath}`
 
-export default Meta;
+    return (
+        <Head>
+            <title itemProp='headline'>{title} | Travel App</title>
+            {description ? (
+                <>
+                    <meta
+                        itemProp='description'
+                        name='description'
+                        content={onlyText(description, 152)}
+                    />
+                    <link rel='canonical' href={currentUrl} />
+                    <meta property='og:locale' content='en' />
+                    <meta property='og:title' content={title} />
+                    <meta property='og:url' content={currentUrl} />
+                    <meta property='og:image' content={image || '/logo.jpg'} />
+                    <meta property='og:site_name' content='Travel App' />
+                    <meta
+                        property='og:description'
+                        content={onlyText(description, 197)}
+                    />
+                </>
+            ) : (
+                <meta name='robots' content='noindex, nofollow' />
+            )}
+        </Head>
+    )
+}
+
+export default Meta
