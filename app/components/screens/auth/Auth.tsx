f4b31673e1f9475ac4f41c9e32966defd20edba8
AnnaDe-v <annade.v88@gmail.com>
2022-05-16 22:05:33 +0300

    auth, react hook form

--- app/components/screens/auth/Auth.tsx
+++ app/components/screens/auth/Auth.tsx
@@ -2,20 +2,44 @@ import React, {FC} from 'react';
 import Layout from "../../common/Layout";
 import {SubmitHandler, useForm} from "react-hook-form";
 import {IAuthFields} from "@/screens//auth/auth.inteerface";
+import styles from '../auth/Auth.module.scss'
+import stylesForButton from '../place/BookTrip/BookTrip.module.scss'
+import {AiFillSecurityScan} from "react-icons/all";
+
 
 const Auth: FC = () => {
     const {handleSubmit, register, formState: {errors}} = useForm<IAuthFields>({
         mode: 'onChange'
     })
-    const onSubmit: SubmitHandler<IAuthFields> = (data) => {
 
-    }
+    const onSubmit: SubmitHandler<IAuthFields> = data => {}
 
 
     return <Layout>
         <h1>Auth/register</h1>
         <form onSubmit={handleSubmit(onSubmit)}>
-
+            <div className={styles.wrapper}>
+                <input
+                    {...register('email', {required: 'Email is required'})}
+                    type="text"
+                    placeholder='email'
+                    className={styles.input}
+                />
+                {errors.email && <div className={styles.error}>{errors.email}</div>}
+            </div>
+            <div className={styles.wrapper}>
+                <input
+                    {...register('password', {required: 'Password is required'})}
+                    type="text"
+                    placeholder='password'
+                    className={styles.input}
+                />
+                {errors.password && <div className={styles.error}>{errors.password}</div>}
+            </div>
+            <button className={stylesForButton.button}>
+                <span className={stylesForButton.text}>Auth</span>
+                <span className={stylesForButton.icon}><AiFillSecurityScan size='18' /></span>
+            </button>
         </form>
     </Layout>
 };
